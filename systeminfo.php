d6ea16e9a8f48fc2614654958a985208eb84af22
n0kS Phr33d0m <god@politeia.in>
2012-07-17 02:03:26 +0200

    Added a couple of things

--- systeminfo.php
+++ systeminfo.php
@@ -3,6 +3,9 @@
  *  ------------------------------------------ 
  *  Author: wutno (#/g/tv - Rizon) 
  *  Last update: 1/4/2012 5:28AM -5GMT (USELESS BLOAT) 
+ *
+ *  Some changes made by n0kS Phr33d0m
+ *  (almost useless changes of course)
  * 
  * 
  *  GNU License Agreement 
@@ -23,11 +26,15 @@
  *  http://www.gnu.org/licenses/gpl-2.0.txt 
  */ 
 
+// --------- EDIT -------------
+$system = "/etc/gentoo-release";
+// ----------------------------
+
 //#Since we all enjoy Open Source 
 if(isset($_GET['dat']) && $_GET['dat'] == "sauce"){ 
     $lines = implode(range(1, count(file(__FILE__))), '<br />'); 
     $content = highlight_file(__FILE__, TRUE); 
-    die('<html><head><title>Page Source For: '.__FILE__.'</title><style type="text/css">body {margin: 0px;margin-left: 5px;}.num {border-right: 1px solid;color: gray;float: left;font-family: monospace;font-size: 13px;margin-right: 6pt;padding-right: 6pt;text-align: right;}code {white-space: nowrap;}td {vertical-align: top;}</style></head><body><table><tr><td class="num"  style="border-left:thin; border-color:#000;">'.$lines.'</td><td class="content">'.$content.'</td></tr></table></body></html>'); 
+    die('<html><head><title>Page Source For: '.__FILE__.'</title><style type="text/css">body {margin: 0px;margin-left: 5px;}.num {border-right: 1px solid;color: gray;float: left;font-family: monospace;font-size: 13px;margin-right: 6pt;padding-right: 6pt;text-align: right;}code {white-space: nowrap;}td {vertical-align: top;}</style></head><body><i>Code also available at <a href="https://github.com/Phr33d0m/Random/blob/master/systeminfo.php">github</a>.</i><table><tr><td class="num"  style="border-left:thin; border-color:#000;">'.$lines.'</td><td class="content">'.$content.'</td></tr></table></body></html>'); 
 } 
 
 function kb2bytes($kb){ 
@@ -35,9 +42,9 @@ function kb2bytes($kb){
 } 
 function format_bytes($bytes){ 
     if ($bytes < 1024){ return $bytes; } 
-    else if ($bytes < 1048576){ return round($bytes / 1024, 2).'KB'; } 
-    else if ($bytes < 1073741824){ return round($bytes / 1048576, 2).'MB'; } 
-    else if ($bytes < 1099511627776){ return round($bytes / 1073741824, 2).'GB'; } 
+    else if ($bytes < 1048576){ return round($bytes / 1024, 2).'KiB'; } 
+    else if ($bytes < 1073741824){ return round($bytes / 1048576, 2).'MiB'; } 
+    else if ($bytes < 1099511627776){ return round($bytes / 1073741824, 2).'GiB'; } 
     else{ return round($bytes / 1099511627776, 2).'TB'; } 
 } 
 function numbers_only($string){ 
@@ -47,16 +54,17 @@ function calculate_percentage($used, $total){
     return @round(100 - $used / $total * 100, 2); 
 } 
 
-//#users, uptime, load avgs 
+//#users, uptime, load avgs, system info
 $uptime = exec('uptime'); 
 preg_match('/ (.+) up (.+) user(.+): (.+)/', $uptime, $update_out); 
 $users_out = substr($update_out[2], strrpos($update_out[2], ' ')+1); 
 $uptime_out = substr($update_out[2], 0, strrpos($update_out[2], ' ')-2); 
 $load_out = str_replace(', ',', <small>',$update_out[4]).str_repeat('</small>',2); 
 
-//#HDD info 
-$harddrive = explode(' ', preg_replace('/\s\s+/', ' ', exec('df /dev/sda1'))); 
-$harddrive_out = format_bytes(kb2bytes($harddrive[2])).'<small> / '.format_bytes(kb2bytes($harddrive[1])).' <small>('.calculate_percentage(kb2bytes($harddrive[2]), kb2bytes($harddrive[1])).'% Free)</small></small>'; 
+//#HDD info from all HDDs availabe (not a very good implementation tho)
+$harddrive_used = exec("df | awk '{ SUM += $3} END { print SUM }'");
+$harddrive_total = exec("df | awk '{ SUM += $2} END { print SUM }'");
+$harddrive_out = format_bytes(kb2bytes($harddrive_used)).'<small> / '.format_bytes(kb2bytes($harddrive_total)).' <small>('.calculate_percentage(kb2bytes($harddrive_used), kb2bytes($harddrive_total)).'% Free)</small></small>'; 
 
 #bandwidth info 
 $vnstat = explode(';', shell_exec('vnstat --oneline')); 
@@ -78,13 +86,13 @@ $memory['Used Swap'] = $memory['Total Swap'] - $memory['Free Swap'];
 $memory['Used RAM'] = $memory['Total RAM'] - $memory['Free RAM'] - $memory['Cached RAM']; 
 $memory['RAM Percent Free'] = calculate_percentage($memory['Used RAM'],$memory['Total RAM']); 
 $memory['Swap Percent Free'] = calculate_percentage($memory['Used Swap'],$memory['Total Swap']); 
-$memory_out = format_bytes($memory['Used RAM']).'<small> / '.format_bytes($memory['Total RAM']).' <small> *'.format_bytes($memory['Cached RAM']).' Cached ('.$memory['RAM Percent Free'].'% Free)</small></small>'; 
+$memory_out = format_bytes($memory['Used RAM']).'<small> / '.format_bytes($memory['Total RAM']).' <small> '.format_bytes($memory['Cached RAM']).' cached ('.$memory['RAM Percent Free'].'% Free)</small></small>'; 
 $swap_out = format_bytes($memory['Used Swap']).'<small> / '.format_bytes($memory['Total Swap']).' <small>('.$memory['Swap Percent Free'].'% Free)</small></small>'; 
 ?> 
 <!DOCTYPE html> 
 <html> 
     <head> 
-        <title>#/g/tv - Shell Server</title> 
+        <title><? echo exec('hostname'); ?> - shell server</title> <!-- Lets hope hostname doesn't return anything unexpected -->  
         <meta name="robots" content="noindex, nofollow, noarchive, nosnippet, noodp" /> <!-- I fucking hate robots... --> 
         <meta name="description" content="Status page for #/g/tv shell server." /> 
         <meta charset="UTF-8" /> 
@@ -98,6 +106,7 @@ $swap_out = format_bytes($memory['Used Swap']).'<small> / '.format_bytes($memory
         </style> 
     </head> 
     <body> 
+	<div id="links">useful <a href="?dat=sauce">sauce</a></div> 
         server time<span><?=date("j-F-Y H:i:s", time());?></span> <!-- Server time is acutally PHP time since PHP wants to bitch --> 
         uptime<span><?=$uptime_out;?></span> <!-- Server uptime -->  
         users logged in<span><?=$users_out;?></span> <!-- Users logged in --> 
@@ -105,7 +114,7 @@ $swap_out = format_bytes($memory['Used Swap']).'<small> / '.format_bytes($memory
         memory<span><?=$memory_out;?></span> <!-- RAM usage --> 
         swap<span><?=$swap_out;?></span> <!-- SWAP usage --> 
         disk<span><?=$harddrive_out;?></span> <!-- Disk information --> 
-        bandwidth used (m)<small></small><span><?=$vnstat[8].$vnstat[9].$vnstat[10].$vnstat[11];?></small></small></span> <!-- Monthly bandwidth usage --> 
-        <small><small>POWERED BY</small> #/g/tv - Rizon</small> 
+        bandwidth used (monthly)<small></small><span><?=$vnstat[8].$vnstat[9].$vnstat[10].$vnstat[11];?></small></small></span> <!-- Monthly bandwidth usage --> 
+        <small><small>powered by</small> <? echo exec('cat '.$system.' | head -1');?></small>
     </body> 
 </html> 
