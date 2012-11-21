ef800ae257bcebb596d10d03e1de6ce755c41106
n0kS Phr33d0m <god@politeia.in>
2012-11-22 00:51:28 +0100

    Add a couple of changes.

--- openmp-mm_mult.c
+++ openmp-mm_mult.c
@@ -18,9 +18,11 @@
 int main(int argc, char *argv[])
 {
     int mat_size, iN, fN, incN;
-	int nprocs, iam, chunk = 10, i , j;
+	int nprocs, iam, chunk = 50;
+	long i, j, k;
 	double *mat1, *mat2, *sol;
 	double time, start, finish, Mflops;
+	time = start = finish = Mflops = 0.;
 
 	if(argc<4) {
 		printf("\n\nUSAGE: %s size_initial size_final size_increment\n\n",argv[0]);
@@ -34,9 +36,11 @@ int main(int argc, char *argv[])
 	for(mat_size=iN; mat_size<=fN; mat_size+=incN) {
 		int mat_size2 = mat_size * mat_size;
 		// Allocating memory to the three matrixes.
-		mat1 = (double *) malloc(mat_size2*10);
-		mat2 = (double *) malloc(mat_size2*10);
-		sol = (double *) malloc(mat_size2*10);
+		mat1 = (double *) malloc(sizeof(double)*mat_size2);
+		mat2 = (double *) malloc(sizeof(double)*mat_size2);
+		sol = (double *) malloc(sizeof(double)*mat_size2);
+		if(mat1==NULL || mat2==NULL || sol==NULL)
+			printf("\nError in Matrix allocation. Ask Morpheus.\n");
 
 		// Generating random values between 0 and 1 for mat1 and mat2.
 		// About sol, we will simply fill it with zeroes.
@@ -52,50 +56,41 @@ int main(int argc, char *argv[])
 		for (i = 0; i < mat_size2; i++)
 			sol[i] = 0;
 
-#if defined (_OPENMP)
 		start = omp_get_wtime();
-#endif
 
 		// note: nprocs is shared so we can use it later
-		#pragma omp parallel shared(mat1, mat2, sol, nprocs) private(iam, i, j)
+		#pragma omp parallel shared(mat1, mat2, sol, nprocs, chunk) private(iam, i, j, k)
 		{
-#if defined (_OPENMP)
 			iam = omp_get_thread_num();
-			if (iam == 0) {
+			if(iam==0)
 				nprocs = omp_get_num_threads();
-			}
-#endif
 
 			// Where the magic occurs.
-			#pragma omp for schedule (static, chunk)
-			for (i = 0; i < mat_size; i++)
+			#pragma omp for schedule (dynamic)
+			for (i = 0; i < mat_size; i++) {
+				// This (long x) optimizes the loops a little.
+				// Test case: mat1(2000x2000) x mat2(2000x2000)
+				//    before: 18.219063
+				//     after: 16.466386 seconds
+				long x = i*mat_size;
 				for (j = 0; j < mat_size; j++)
-					sol[i*mat_size+j] += mat1[i*mat_size+j] * mat2[j*mat_size+i];
+					for (k = 0; k < mat_size; k++)
+						sol[x+j] += mat1[x+k] * mat2[k*mat_size+j];
+			}
 		}
 
-#if defined (_OPENMP)
 		finish = omp_get_wtime();
-#endif
 
 		time = finish - start;
 		if(time==0.) {
-			printf("No hay suficiente precision\n");
+			printf("\nNot enough precission.\n");
 		}
 		else {
 			Mflops=((2.*mat_size2)/time)/1000000.;
 			printf("\n>>> Threads = %d\t\tSize = %d\t\tSeconds = %.6lf", nprocs, mat_size, time);
 			printf("\n> Mflops = %.6lf\t\tMflops/thread = %.6lf",Mflops,Mflops/nprocs);
-#if defined (_OPENMP)
 			printf("\n> Precision omp_get_wtick = %lf\n",omp_get_wtick());
-#endif
 		}
-
-		/* For testing purposes only.
-		for (i = 0; i < mat_size; i++) {
-			for (j = 0; j < mat_size; j++)
-				printf("\t%f",sol[i*mat_size+j]);
-			printf("\n");
-		}*/
 
 		// Free memory like a boss.
 		free(mat1);
