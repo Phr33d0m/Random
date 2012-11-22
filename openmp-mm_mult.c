edb18123d1be0cb5938aa179d1119edc40a50059
n0kS Phr33d0m <god@politeia.in>
2012-11-22 03:27:52 +0100

    Some rewrite here and there.

--- openmp-mm_mult.c
+++ openmp-mm_mult.c
@@ -17,12 +17,10 @@
 
 int main(int argc, char *argv[])
 {
-    int mat_size, iN, fN, incN;
-	int nprocs, iam, chunk = 50;
-	long i, j, k;
-	double *mat1, *mat2, *sol;
-	double time, start, finish, Mflops;
-	time = start = finish = Mflops = 0.;
+    short iN, fN, incN;
+	int nprocs = 0, iam = 0, mat_size, i, j, k;
+	float time, start, finish;
+	double *mat1, *mat2, *sol, mflops;
 
 	if(argc<4) {
 		printf("\n\nUSAGE: %s size_initial size_final size_increment\n\n",argv[0]);
@@ -35,7 +33,8 @@ int main(int argc, char *argv[])
 
 	for(mat_size=iN; mat_size<=fN; mat_size+=incN) {
 		int mat_size2 = mat_size * mat_size;
-		// Allocating memory to the three matrixes.
+
+		// Allocating memory to the three matrix.
 		mat1 = (double *) malloc(sizeof(double)*mat_size2);
 		mat2 = (double *) malloc(sizeof(double)*mat_size2);
 		sol = (double *) malloc(sizeof(double)*mat_size2);
@@ -44,51 +43,74 @@ int main(int argc, char *argv[])
 
 		// Generating random values between 0 and 1 for mat1 and mat2.
 		// About sol, we will simply fill it with zeroes.
-		#pragma omp for schedule (dynamic, chunk)
-		for (i = 0; i < mat_size2; i++)
+		#pragma omp for schedule (dynamic, 20)
+		for (i = 0; i < mat_size2; i++) {
 			mat1[i] = (double)rand()/RAND_MAX;
-
-		#pragma omp for schedule (dynamic, chunk)
-		for (i = 0; i < mat_size2; i++)
 			mat2[i] = (double)rand()/RAND_MAX;
-
-		#pragma omp for schedule (dynamic, chunk)
-		for (i = 0; i < mat_size2; i++)
 			sol[i] = 0;
+		}
 
-		start = omp_get_wtime();
-
-		// note: nprocs is shared so we can use it later
-		#pragma omp parallel shared(mat1, mat2, sol, nprocs, chunk) private(iam, i, j, k)
+		// note: nprocs is shared because we need it for later use.
+		#pragma omp parallel shared(nprocs) private(iam)
 		{
-			iam = omp_get_thread_num();
-			if(iam==0)
-				nprocs = omp_get_num_threads();
+		iam=omp_get_thread_num();
+		if(iam==0)
+			nprocs=omp_get_num_threads();
+		}
 
-			// Where the magic occurs.
-			#pragma omp for schedule (dynamic)
+		// This block contains the matrix multiplication code.
+		{
+			// This (long x) optimizes the loops a little.
+			// Test case: mat1(2000x2000) x mat2(2000x2000)
+			//   threads: 8
+			// -------------------------------
+			//    before: 18.219063 seconds
+			//     after: 16.466386 seconds
+			// -------------------------------
+			//  Speed up: 10.64%
+			long x;
+			double temp;
+
+			// Clock START.
+			start = omp_get_wtime();
+
+			// Where the magic begins.
+			#pragma omp parallel for schedule(runtime)
 			for (i = 0; i < mat_size; i++) {
-				// This (long x) optimizes the loops a little.
-				// Test case: mat1(2000x2000) x mat2(2000x2000)
-				//    before: 18.219063
-				//     after: 16.466386 seconds
-				long x = i*mat_size;
-				for (j = 0; j < mat_size; j++)
+				x = i*mat_size;
+				for (j = 0; j < mat_size; j++) {
+					// Using 'reduction' with a temp variable optimizes the calculation a lot!
+					// Test case: mat1(2000x2000) x mat2(2000x2000)
+					//   threads: 8
+					// -------------------------------
+					//    before: 16.466386 seconds
+					//     after: 10.878906 seconds
+					// -------------------------------
+					//  Speed up: 51.36%
+					temp = 0;
+					#pragma omp parallel for reduction(+:temp)
 					for (k = 0; k < mat_size; k++)
-						sol[x+j] += mat1[x+k] * mat2[k*mat_size+j];
+						temp = mat1[x+k] * mat2[k*mat_size+j];
+					sol[x+j] = temp;
+				}
 			}
 		}
 
+		// Clock STOP.
 		finish = omp_get_wtime();
 
 		time = finish - start;
+
 		if(time==0.) {
 			printf("\nNot enough precission.\n");
 		}
 		else {
-			Mflops=((2.*mat_size2)/time)/1000000.;
+			// >>> mflops = (operations/time)/1000000
+			// > operations = (first for()) + (second for()) + (third for())
+			// (third for()) = (2*mat_size) <- because we have two floating point operations
+			mflops = ((double)mat_size2*(2.*(double)mat_size)/time)/1000000.;
 			printf("\n>>> Threads = %d\t\tSize = %d\t\tSeconds = %.6lf", nprocs, mat_size, time);
-			printf("\n> Mflops = %.6lf\t\tMflops/thread = %.6lf",Mflops,Mflops/nprocs);
+			printf("\n> Mflops = %.6f\t\tMflops/thread = %.6f",mflops,mflops/nprocs);
 			printf("\n> Precision omp_get_wtick = %lf\n",omp_get_wtick());
 		}
 
